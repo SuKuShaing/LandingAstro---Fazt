f63948a2f3dd7992e224e467a96ecf821a5361b5
Sebastian <sebastian.sanhueza.t@usach.cl>
2024-11-14 14:42:48 -0300

    ahora si carga plz

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://SuKuShaing.github.io',
-  base: '/LandingAstro---Fazt',
+  base: 'LandingAstro---Fazt',
 });
